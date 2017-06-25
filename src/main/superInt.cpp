ebb37481d959e1142071bb3e7bc7b6e8b0b7c86c
Michał Szczerba <pan.szczerba@gmail.com>
2017-06-25 14:47:25 +0200

    Changes to be committed: 	modified:   src/main/superInt.cpp 	modified:   src/main/superInt.h

--- src/main/superInt.cpp
+++ src/main/superInt.cpp
@@ -1,1 +1,19 @@
+#include <cstring>
 #include "superInt.h"
+
+superInt::superInt(long long int number)
+{
+    tblptr = new long long int[1];
+    length = 1;
+    tblptr[0] = number;
+}
+superInt::superInt(superInt& other)
+{
+    length = other.getLength();
+    tblptr = new long long int[length];
+    memcpy(tblptr, other.tblptr, length*sizeof(long long int));
+}
+superInt::~superInt()
+{
+    delete tblptr;
+}
--- src/main/superInt.h
+++ src/main/superInt.h
@@ -1,11 +1,16 @@
 #ifndef _SUPER_INT_H
 #define _SUPER_INT_H
 
+#include <cstddef>
+
 class superInt
 {
-    superInt();
-    superInt(int);
-    superInt(&superInt);
+    long long int* tblptr;
+    std::size_t length;
+public:
+    superInt(long long int = 0);
+    superInt(superInt&);
     ~superInt();
+    inline std::size_t getLength() { return length; }
 };
 #endif // _SUPER_INT_H
